744d56e4205399d119353ce89b56632b43ec9c5e
john <john27328@gmail.com>
2019-12-07 20:45:34 +0300

    декодирование хоффмана, ввод

---
+++ de_hoffman.py
@@ -0,0 +1,13 @@
+#nChar, lenH = (int(i) for i in input().split())
+#
+#codeList = []
+#for i in range(nChar):
+#    s, code = input().split(':')
+#    codeList.append([s, code])
+#    
+#codeStr = input()
+
+nChar = 1
+lenH = 1
+codeList = [['a', '0'],]
+codeStr = ["0",]
